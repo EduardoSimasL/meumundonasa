87baaa0333c12e4f164c9398446307cb6e96e3e7
Eduardo Simas <92694555+Suvelinha@users.noreply.github.com>
2023-10-08 16:11:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Encontrar o elemento de link
-    var shareLink = document.querySelector('li a[href="#secao1"]');
+    // Função para suavizar o scroll
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-    // Adicionar um ouvinte de clique ao link
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Encontrar os elementos de link
+    var shareLink = document.querySelector('li a[href="#secao1"]');
+    var interactivesLink = document.querySelector('li a[href="#secao2"]');
+    var learningsLink = document.querySelector('li a[href="#secao3"]');
+
+    // Adicionar um ouvinte de clique ao link "Share stories with the world"
     if (shareLink) {
         shareLink.addEventListener('click', function (event) {
-            // Impedir o comportamento padrão de seguir o link
             event.preventDefault();
-
-            // Redirecionar para a página desejada
-            window.location.href = 'Page.html';
+            window.location.href = 'index.html';
         });
     }
+
+    // Adicionar um ouvinte de clique ao link "Interactive content"
+    if (interactivesLink) {
+        interactivesLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.location.href = 'https://www.nasa.gov/interactives/';
+        });
+    }
+
+    // Adicionar um ouvinte de clique ao link "Learnings"
+    if (learningsLink) {
+        learningsLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.location.href = 'Learnings.html';
+        });
+    }
+
+    // Adicionar evento de clique às clickable-content
+    document.querySelectorAll('.clickable-content').forEach(clickableContent => {
+        clickableContent.addEventListener('click', function () {
+            // Obter o link dentro da clickable-content
+            const link = this.querySelector('a');
+
+            // Verificar se o link existe e simular o clique
+            if (link) {
+                link.click();
+            }
+        });
+    });
 });
